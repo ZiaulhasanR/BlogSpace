a2714aa7a0224d659c614365ee8ff0d0bcd5b3ff
ziaul-hassan <ziaulhasanf@gmail.com>
2025-03-06 09:37:49 +0600

    my post in navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -46,7 +46,15 @@
                     <!-- Dropdown Menu -->
                     <div x-show="open" @click.away="open = false"
                         class="absolute right-0 mt-2 w-40 bg-white border rounded-lg shadow-lg">
-                        <a href="#" class="block px-4 py-2 text-gray-700 hover:bg-gray-200">Profile</a>
+                        {{-- <a href="#" class="block px-4 py-2 text-gray-700 hover:bg-gray-200">My Posts</a> --}}
+                        @if (Auth::check())
+                        @php $role = Auth::user()->role; @endphp
+                        @if ($role === 'author' || $role === 'admin')
+                            <a href="{{ route('posts.my') }}" class="block px-4 py-2 text-gray-700 hover:bg-gray-200">
+                                My posts
+                            </a>
+                        @endif
+                    @endif
                         <form method="POST" action="{{ route('logout') }}" class="block">
                             @csrf
                             <button type="submit" class="w-full text-left px-4 py-2 text-gray-700 hover:bg-gray-200">
